a2124fdb46b2dd40d691755991ec807e461bd087
thuiskon <tuco.kerber@gmail.com>
2021-03-23 00:43:46 -0300

    comit05_alteracoes Carlos

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ class App extends Component {
     }
     e.preventDefault();
   }
+
   formataResult(imc) {
 
     let valorTotal = imc.toString().replace(".", ",");
@@ -60,6 +61,7 @@ class App extends Component {
     this.setState({ imc: valorTotal });
 
     this.formatMsg(imc);
+
   }
 
   formatMsg(imc) {
@@ -68,22 +70,22 @@ class App extends Component {
       let text = `Cuidado, você está abaixo do peso ideal`;
       this.setState({ msg: text });
     }
-    else if ((imc > 18.5) && (imc < 24.9)) {
+    else if ((imc > 18.5) && (imc <= 25)) {
       let text = `Parabéns! você está dentro da faixa ideal de peso`;
       this.setState({ msg: text });
 
     }
-    else if ((imc > 24.9) && (imc < 26)) {
+    else if ((imc > 25) && (imc < 25.9)) {
       let text = `Você está com sobrebeso`;
       this.setState({ msg: text });
       
     }
-    else if ((imc > 25.9) && (imc < 35)) {
+    else if ((imc > 25.9) && (imc < 34.9)) {
       let text = `Infelizmente você está com obesidade nível 1`;
       this.setState({ msg: text });
       
     }
-    else if (imc > 34.9 && imc < 40) {
+    else if (imc > 34.9 && imc < 39.9) {
       let text = `Cuidado! você está com obesidade nível 2`;
       this.setState({ msg: text });
       
@@ -105,31 +107,34 @@ class App extends Component {
     let acumulator = 0;
     let imc2 = imc;
     let pesoIdeal = 0;
+    let nPeso = 0;
+    
 
-    if (imc2 > 24.99) {
+    if (imc2 >= 25) {
       while (imc2 > 25) {
-        let nPeso = peso - acumulator;
+        nPeso = peso - acumulator;
 
         imc2 = nPeso / (altura * altura);
         acumulator = acumulator + 0.1;
         this.setState({ acumulator });
 
       }
-      let pesoAdd = acumulator.toFixed(1);
-      pesoIdeal = peso - pesoAdd;
+      let pesoAdd = nPeso.toFixed(1);
+      pesoIdeal = pesoAdd;
     }
-    else if (imc2 < 24.99) {
+    else if (imc2 <= 24.9) {
       while (imc2 < 25) {
-        let nPeso = peso - acumulator;
+        nPeso = peso - acumulator;
 
         imc2 = nPeso / (altura * altura);
         acumulator = acumulator - 0.1;
         this.setState({ acumulator });
       }
-      let pesoAdd = acumulator.toFixed(1);
-      pesoIdeal = peso - pesoAdd;
+      let pesoAdd = nPeso.toFixed(1);
+      pesoIdeal = pesoAdd;
     }
-
+    
+    console.log(this.state.peso);
     this.setState({ pesoIdeal });
 
   }
@@ -159,7 +164,8 @@ class App extends Component {
           <div className="result">
             <h3>IMC: {this.state.imc}</h3>
             <p>{this.state.msg}</p>
-            <p>Seu peso ideal é: {this.state.pesoIdeal} Kg</p>
+             
+              {(this.state.imc === '25,0') ?  <p>Este é seu peso ideal</p> : <p>Seu peso ideal é: {this.state.pesoIdeal}Kg</p>}
           </div>
           :
           <div className="result">
